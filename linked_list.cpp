cc340c5a8f4c205fca7251e20b8e0025e2138c4c
Sukuna <inxraunakpro10@gmail.com>
2025-05-17 15:02:59 +0530

    Linked Lists Updated and compiled

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,12 +1,18 @@
+#include <cstddef>
 #include<iostream>
 
-template<typename T>
+template<typename _type>
 class node{
     private:
         node *m_next;
-        T m_data;
+        _type m_data;
     public:
-        T getData(){
+        node(): m_next(nullptr),m_data(){}
+        node(node<_type>& other){
+            other.m_data = m_data;
+            other.m_next = m_next;
+        }
+        _type getData(){
             return m_data;
         }
         node *next(){
@@ -14,15 +20,29 @@ class node{
         }
 };
 
-template<typename T>
+template<typename _type>
 class l_list{
     private:
-        node<T> head;
+        node<_type> head;
+        node<_type> tail;
+        int m_size;
+
+    public:
+        l_list(): m_size{0} {}
+        
+        l_list(l_list<_type>& other){
+            other.tail = this->tail;
+            other.head = this->head;
+            other.m_size = this->m_size;
+        }
 
     public:
 
-        T show_data(){
+        _type show_data(){
             return head.getData();
         }
-        
 };
+
+int main(){
+    l_list<int> llist;
+}
